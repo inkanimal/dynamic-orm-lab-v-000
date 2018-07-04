e8f7e575c9f110e60f59c1101dc0be64958294fc
geoffrey woodcock <theatomproject@gmail.com>
2018-07-04 13:44:57 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -54,7 +54,7 @@ class InteractiveRecord
    end
    
    def self.find_by(attribute)
-    sql = "SELECT * FROM #{self.table_name} WHERE column =
+    sql = "SELECT * FROM #{self.table_name} WHERE column_names =
      '#{attribute}'"
      DB[:conn].execute(sql)
    end
